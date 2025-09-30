58d38392530a6d27fa8306e58050ad39c6191100
rojin <28rozhin@gmail.com>
2025-09-29 16:01:16 -1100

    Initial commit - clean history

---
+++ members/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class Member(models.Model):
+    firstname = models.CharField(max_length=255)
+    lastname = models.CharField(max_length=255)
+    phone = models.IntegerField(null=True)
+    joined_date = models.DateField(null=True)
+
+    # Change the string representation of objects in model
+    # def __str__(self):
+    #   return f"{self.firstname} {self.lastname}"
